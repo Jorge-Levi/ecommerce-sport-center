819a8524d750050c21163aed9f44ffc8eb5e23c2
Jorge-Levi <tapialugardo29@gmail.com>
2025-12-04 21:40:40 -0600

    creating first entity Brand

---
+++ src/main/java/com/ecommerce/sportscenter/entity/Brand.java
@@ -0,0 +1,22 @@
+package com.ecommerce.sportscenter.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Brand")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "Name")
+    private String name;
+}
